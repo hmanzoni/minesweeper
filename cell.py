224b680aac7530d0acbd3c9c8b3d31c5d8ba5ec1
hugo cm <hugocmdesign@gmail.com>
2023-04-04 21:01:12 +0200

    know the surrounded cells of cell clicked

--- cell.py
+++ cell.py
@@ -17,20 +17,46 @@ class Cell:
         btn = Button(
             location, 
             width=12, 
-            height=4, 
-            text=f"{self.x},{self.y}"
+            height=4
         )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
     
     def left_click_actions(self, event):
-        print(event)
-        print("I am left clicked!")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
     
     def right_click_actions(self, event):
         print(event)
         print("I am right clicked!")
+
+    def show_mine(self):
+        # A logic to interrupt the game 
+        # and display a msg to the player
+        self.cell_btn_object.configure(bg='red')
+
+    def get_cell_by_axis(self, x, y):
+        # Return a cell object based on the value of x,y
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+    
+    def show_cell(self):
+        surrounded_cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+
+        surrounded_cells = [cell for cell in surrounded_cells if cell is not None]
 
     @staticmethod
     def randomize_mines():
