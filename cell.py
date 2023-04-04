2132c0a7614a36f9fd8ce2dade3192b6aaf6965c
hugo cm <hugocmdesign@gmail.com>
2023-04-04 20:37:20 +0200

    add dynamic text to btn and change print info obj

--- cell.py
+++ cell.py
@@ -1,12 +1,25 @@
 from tkinter import Button
+import random
+import settings
 
 class Cell:
-    def __init__(self, is_mine=False):
+    all = []
+    def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
+        self.x = x
+        self.y = y
+
+        # Append the object to the Cell.all list
+        Cell.all.append(self)
     
     def create_btn_object(self, location):
-        btn = Button(location, bg='blue', text='Text')
+        btn = Button(
+            location, 
+            width=12, 
+            height=4, 
+            text=f"{self.x},{self.y}"
+        )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
@@ -18,3 +31,6 @@ class Cell:
     def right_click_actions(self, event):
         print(event)
         print("I am right clicked!")
+
+    def __repr__(self):
+        return f"Cell({self.x}, {self.y})"
