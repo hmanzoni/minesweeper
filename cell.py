147827124cf219cba12de7d9134587808b9db857
hugo cm <hugocmdesign@gmail.com>
2023-04-06 20:54:35 +0200

    Fn for colored cell as mines candidates

--- cell.py
+++ cell.py
@@ -10,6 +10,7 @@ class Cell:
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.is_opened = False
+        self.is_mine_candidate = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
@@ -48,8 +49,12 @@ class Cell:
             self.show_cell()
     
     def right_click_actions(self, event):
-        print(event)
-        print("I am right clicked!")
+        if not self.is_mine_candidate:
+            self.cell_btn_object.configure(bg='orange')
+            self.is_mine_candidate = True
+        else:
+            self.cell_btn_object.configure(bg='SystemButtonFace')
+            self.is_mine_candidate = False
 
     def show_mine(self):
         # A logic to interrupt the game 
