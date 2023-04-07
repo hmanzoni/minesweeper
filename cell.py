e18f628e7597c4c3e291d0b3fb9fab0f1b673689
hugo cm <hugocmdesign@gmail.com>
2023-04-07 19:21:08 +0200

    unbind button actions when is opened

--- cell.py
+++ cell.py
@@ -49,6 +49,9 @@ class Cell:
                 for cell_obj in self.surrounded_cells:
                     cell_obj.show_cell()
             self.show_cell()
+        # Cancel left and right click events if cell is already opened
+        self.cell_btn_object.unbind('<Button-1>')
+        self.cell_btn_object.unbind('<Button-3>')
     
     def right_click_actions(self, event):
         if not self.is_mine_candidate:
